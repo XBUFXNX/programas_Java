f6259e683410742d563dfc8e8fe06470dce1397b
XBUFXNX <130572327+XBUFXNX@users.noreply.github.com>
2023-08-16 09:57:18 -0300

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,19 @@
+
+public class Ejercicio1 {
+
+
+    	System.out.print(" a continuacion se imprimiran todos los numeros del 1 al 100, aclarando los que son pares");
+    	System.out.print(" \n ");
+            
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i);
+            if (i % 2 == 0) { // Verificar si el número es par
+                System.out.print(" (par)");
+            }
+            if (i < 100) {
+                System.out.print(", ");
+            }
+            System.out.print(" \n ");
+        }
+	
+}
