5c5b16011a6b0db575d25cd7d8669f91d2311843
F.Ayala <130572327+XBUFXNX@users.noreply.github.com>
2023-08-22 18:22:26 -0300

    Add files via upload

---
+++ prog 5.java
@@ -0,0 +1,12 @@
+// ciclo for each
+package CiclosFor; 
+
+import java.util.ArrayList;
+
+public class foreach { 
+	public static void main(String[] args) { 
+		int[] valor = {1, 2, 3, 4, 5, 6, 7, 19, 10}; //array de numeros/int, con varios valores de prueba
+		for(int i : valor ) System.out.println(i); //ciclo for each que le asigna los valores que se van recorriendo a la variable i
+		
+	}
+}
