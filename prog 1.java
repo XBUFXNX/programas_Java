77135e32c14b48a8f288550af11ba32fec958213
Rita Fasolino <84328521+rfasolino@users.noreply.github.com>
2023-08-22 17:59:33 -0300

    Update prog 1.java

--- prog 1.java
+++ prog 1.java
@@ -1,7 +1,6 @@
 
 public class Ejercicio1 {
 
-
     	System.out.print(" a continuacion se imprimiran todos los numeros del 1 al 100, aclarando los que son pares");
     	System.out.print(" \n ");
             
